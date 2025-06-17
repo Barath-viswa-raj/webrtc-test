baab987218fae5f561baf8ce27cb068e0d847045
S.Barath Viswa Raj <135594826+Barath-viswa-raj@users.noreply.github.com>
2025-06-17 15:04:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: "*" },
+});
+
+io.on('connection', socket => {
+  console.log('User connected:', socket.id);
+
+  socket.on('start-call', () => {
+    socket.broadcast.emit('start-call');
+  });
+
+  socket.on('offer', (data) => {
+    socket.broadcast.emit('offer', data);
+  });
+
+  socket.on('answer', (data) => {
+    socket.broadcast.emit('answer', data);
+  });
+
+  socket.on('ice-candidate', (data) => {
+    socket.broadcast.emit('ice-candidate', data);
+  });
+
+  socket.on('end-call', () => {
+    socket.broadcast.emit('end-call');
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+server.listen(5000, () => {
+  console.log('Socket server running on port 5000');
+});
