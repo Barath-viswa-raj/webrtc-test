10a790b033b18a5cbadcedc52e788c862c2a4bf3
S.Barath Viswa Raj <135594826+Barath-viswa-raj@users.noreply.github.com>
2025-06-17 16:00:11 +0530

    index.js

--- index.js
+++ index.js
@@ -1,37 +1,33 @@
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
 
 const app = express();
 const server = http.createServer(app);
+
 const io = new Server(server, {
-  cors: { origin: "*" },
+  cors: { origin: '*' }
 });
 
-io.on("connection", (socket) => {
-  console.log("User connected");
-
-  socket.on("join-room", (roomId) => {
+io.on('connection', (socket) => {
+  socket.on('join-room', (roomId) => {
     socket.join(roomId);
-    socket.to(roomId).emit("user-joined", socket.id);
+    socket.to(roomId).emit('user-joined', socket.id);
 
-    socket.on("offer", (data) => {
-      socket.to(roomId).emit("offer", data);
+    socket.on('offer', (data) => {
+      socket.to(roomId).emit('offer', { ...data, from: socket.id });
     });
 
-    socket.on("answer", (data) => {
-      socket.to(roomId).emit("answer", data);
+    socket.on('answer', (data) => {
+      socket.to(roomId).emit('answer', data);
     });
 
-    socket.on("ice-candidate", (data) => {
-      socket.to(roomId).emit("ice-candidate", data);
-    });
-
-    socket.on("disconnect", () => {
-      console.log("User disconnected");
-      socket.to(roomId).emit("user-left");
+    socket.on('ice-candidate', (data) => {
+      socket.to(roomId).emit('ice-candidate', data);
     });
   });
 });
 
-server.listen(5000, () => console.log("Server running on port 5000"));
+server.listen(process.env.PORT || 5000, () =>
+  console.log('Server running'))
